fb19b3dac8ea4f79402bcd4ea4d05a7402b0309e
Brian Herbert <bherbert@myyearbook.com>
2016-08-29 09:23:02 -0400

    body parser init
    Change-Id: I87eb8f2765962aeeacb4d28c84242fd5b9b2efa5

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ var express = require('express');
 var bodyParser = require('body-parser');
 
 var app = express();
-app.use(bodyParser.urlencoded());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.post('/', function(req, res) {
     console.log('Received POST', req.headers, req.body);
